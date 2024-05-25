6ab8e50a6b3e30380ca39b6fb73da670f15526a9
Jose Lucas <joselucas.dev@gmail.com>
2024-05-25 11:12:55 -0300

    refactor:refactor contract findByEmail

--- src/repositories/implementations/PostgressUserRespository.ts
+++ src/repositories/implementations/PostgressUserRespository.ts
@@ -4,7 +4,7 @@ import { IUserRepository } from "../user-repository";
 export class PostgressUserRepository implements IUserRepository {
   private users: User[] = [];
 
-  async findByEmail(email: string): Promise<User> {
+  async findByEmail(email: string): Promise<User | undefined> {
     const user = this.users.find((user) => user.email === email);
 
     return user;
